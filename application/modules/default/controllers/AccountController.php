f6e701c501a4d8db656d17de2db16dff6a87976d
chanhduypq <chanhduypq@gmail.com>
2018-06-23 12:19:57 +0700

    no message

---
+++ application/modules/default/controllers/AccountController.php
@@ -0,0 +1,45 @@
+<?php
+
+class AccountController extends Core_Controller_Action 
+{
+
+    public function init() 
+    {
+        parent::init();
+        $this->view->headTitle('Trang chủ', true);
+    }
+
+    
+    public function loginAction() 
+    {
+        $auth = Zend_Auth::getInstance();
+        if ($auth->hasIdentity()) {
+            $this->_helper->redirector('index', 'index', 'default');
+            exit;
+        }
+    }
+    
+    public function registAction() 
+    {
+        $auth = Zend_Auth::getInstance();
+        if ($auth->hasIdentity()) {
+            $this->_helper->redirector('index', 'index', 'default');
+            exit;
+        }
+    }
+
+    public function logoutAction() 
+    {
+        $auth = Zend_Auth::getInstance();
+        $auth->clearIdentity();
+        $this->_helper->redirector('index', 'index', 'default');
+    }
+    
+    public function logoutajaxAction() 
+    {
+        $auth = Zend_Auth::getInstance();
+        $auth->clearIdentity();
+        exit;
+    }
+
+}
