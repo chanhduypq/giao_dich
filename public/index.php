2ad5da05dad2ea251c49a0718d32fb9b4b946696
chanhduypq <chanhduypq@gmail.com>
2018-07-29 20:35:24 +0700

    no message

--- public/index.php
+++ public/index.php
@@ -27,7 +27,7 @@ for ($i = 0, $n = count($temp) - 1; $i < $n; $i++) {
         $upload .= "\\" . $temp[$i];
     }
 }
-define('UPLOAD', $upload);
+define('UPLOAD', get_upload_path(APPLICATION_PATH));
 //echo UPLOAD;exit;
 
 /** Zend_Application */
@@ -44,3 +44,52 @@ require_once 'Core.php';
 //} catch (Exception $ex) {
 //    echo '<div>Vui lòng nhấn tại <a href="/">đây</a> để quay về trang chủ</div>';
 //}
+
+    
+function get_upload_path($application_path) {
+    if (!is_string($application_path) || trim($application_path) == "") {
+        return "";
+    }
+    $upload = "";
+    if (System::getOS() == System::OS_WIN) {
+        $temp = explode("\\", $application_path);
+        for ($i = 0, $n = count($temp) - 1; $i < $n; $i++) {
+            if ($i == 0) {
+                $upload.=$temp[$i];
+            } else {
+                $upload.="\\" . $temp[$i];
+            }
+        }        
+    } else if (System::getOS() == System::OS_LINUX) {
+        $temp = explode("/", $application_path);
+        for ($i = 0, $n = count($temp) - 1; $i < $n; $i++) {
+            if ($i == 0) {
+                $upload.=$temp[$i];
+            } else {
+                $upload.="/" . $temp[$i];
+            }
+        }       
+    }
+    return $upload;
+}
+
+class System {
+
+    const OS_UNKNOWN = 1;
+    const OS_WIN = 2;
+    const OS_LINUX = 3;
+    const OS_OSX = 4;
+
+    /**
+     * @return int
+     */
+    static public function getOS() {
+        switch (true) {
+            case stristr(PHP_OS, 'DAR'): return self::OS_OSX;
+            case stristr(PHP_OS, 'WIN'): return self::OS_WIN;
+            case stristr(PHP_OS, 'LINUX'): return self::OS_LINUX;
+            default : return self::OS_UNKNOWN;
+        }
+    }
+
+}
